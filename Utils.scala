0cdb3b28dfb1d68c5a7fefd56af39173b765a923
Luca Spadoni <44862461+LucaSpadoni@users.noreply.github.com>
2024-05-25 19:18:38 +0200

    Delete Utils.scala

--- Utils.scala
+++
@@ -1,9 +0,0 @@
-object Utils {
-  def measureTime[T](block: => T): (T, Double) = {
-    val startTime = System.currentTimeMillis()
-    val result = block
-    val endTime = System.currentTimeMillis()
-    val elapsedTime = (endTime - startTime) / 1000.0 // Time in seconds
-    (result, elapsedTime)
-  }
-}
